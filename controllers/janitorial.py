4ce664a1568c77c38a1c40ab7194bd08c652fa2d
Orlok97 <93604560+Orlok97@users.noreply.github.com>
2024-10-03 22:48:46 +0000

    fix: corrigir função create_request

--- controllers/janitorial.py
+++ controllers/janitorial.py
@@ -7,89 +7,114 @@ from controllers.auth import auth_user
 from datetime import datetime
 from werkzeug.utils import secure_filename
 
-janitorial_bp=Blueprint('janitorial_bp',__name__)
+janitorial_bp = Blueprint('janitorial_bp', __name__)
 
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
 def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-  
-@janitorial_bp.route('/',methods=['GET'])
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def janitorial_to_dict(janitorial):
+    return {
+        'id': janitorial.id,
+        'rua': janitorial.rua,
+        'bairro': janitorial.bairro,
+        'area': janitorial.area,
+        'cep': janitorial.cep,
+        'servico': janitorial.servico,
+        'desc': janitorial.desc,
+        'anexo': janitorial.anexo,
+        'protocolo': janitorial.protocolo,
+        'data': janitorial.data,
+        'hora': janitorial.hora,
+        'user_id': janitorial.user_id
+    }
+
+@janitorial_bp.route('/', methods=['GET'])
 @jwt_required()
 def get_requests():
-  reqs=db.session.execute(db.select(Janitorial).order_by(Janitorial.id)).scalars()
-  request_list=[]
-  for req in reqs:
-    request_list.append(req)
-  return jsonify(request_list)
+    reqs = db.session.execute(db.select(Janitorial).order_by(Janitorial.id)).scalars()
+    request_list = [janitorial_to_dict(req) for req in reqs]
+    return jsonify(request_list)
 
-@janitorial_bp.route('/',methods=['POST'])
+@janitorial_bp.route('/', methods=['POST'])
 @jwt_required()
 def create_request():
-  request_data=request.get_json()
-  now=datetime.now()
-  user=auth_user()
-  file=request.files['file']
-  if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join('uploads/', filename))
-  try:
-    req = Janitorial(
-    rua=request_data['rua'],
-    bairro=request_data['bairro'],
-    area=request_data['area'],
-    cep=request_data['cep'],
-    servico=request_data['servico'],
-    desc=request_data['desc'],
-    anexo=filename,
-    protocolo=request_data['protocolo'],
-    data=now.strftime('%d/%m/%Y'),
-    hora=now.strftime('%H:%M:%S'),
-    user_id=user.id
-    )
-    db.session.add(req)
-    db.session.commit()
-    print(user.nome)
-  except Exception as e:
-    return jsonify({'error':str(e)})
-  return jsonify({'response':'servico solicotado'})
+    request_data = request.form.to_dict()
+    now = datetime.now()
+    user = auth_user()
 
-@janitorial_bp.route('/<int:id>',methods=['GET'])
+    file = request.files['file']
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+
+
+        if not os.path.exists('uploads/'):
+            os.makedirs('uploads/')
+
+        file.save(os.path.join('uploads/', filename))
+    else:
+        filename = None
+
+    try:
+        req = Janitorial(
+            rua=request_data['rua'],
+            bairro=request_data['bairro'],
+            area=request_data['area'],
+            cep=request_data['cep'],
+            servico=request_data['servico'],
+            desc=request_data['desc'],
+            anexo=filename,
+            protocolo=request_data['protocolo'],
+            data=now.strftime('%d/%m/%Y'),
+            hora=now.strftime('%H:%M:%S'),
+            user_id=user.id
+        )
+        db.session.add(req)
+        db.session.commit()
+        print(user.nome)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+
+    return jsonify({'response': 'Serviço solicitado'})
+
+@janitorial_bp.route('/<int:id>', methods=['GET'])
 @jwt_required()
 def get_request(id):
-  try:
-    request=db.get_or_404(Janitorial,id)
-  except Exception as e:
-    return jsonify({'response:':str(e)}),404
-  return jsonify(request)
+    try:
+        janitorial_request = db.get_or_404(Janitorial, id)
+    except Exception as e:
+        return jsonify({'response': str(e)}), 404
+    return jsonify(janitorial_to_dict(janitorial_request))
 
-@janitorial_bp.route('/<int:id>',methods=['PUT'])
+@janitorial_bp.route('/<int:id>', methods=['PUT'])
 @jwt_required()
 def update_request(id):
-  request_data=request.get_json()
-  try:
-    req=db.get_or_404(Janitorial,id)
-    req.rua=request_data['rua']
-    req.bairro=request_data['bairro']
-    req.area=request_data['area']
-    req.cep=request_data['cep']
-    req.servico=request_data['servico']
-    req.desc=request_data['desc']
-    req.anexo=request_data['anexo']
-    req.protocolo=request_data['protocolo']
-    db.session.add(req)
-    db.session.commit()
-  except Exception as e:
-    return jsonify({'response':str(e)})
-  return jsonify({'response':'solicitacao alterada'})
-  
-@janitorial_bp.route('/<int:id>',methods=['DELETE'])
+    request_data = request.get_json()
+    try:
+        req = db.get_or_404(Janitorial, id)
+        req.rua = request_data['rua']
+        req.bairro = request_data['bairro']
+        req.area = request_data['area']
+        req.cep = request_data['cep']
+        req.servico = request_data['servico']
+        req.desc = request_data['desc']
+        req.anexo = request_data['anexo']
+        req.protocolo = request_data['protocolo']
+        db.session.add(req)
+        db.session.commit()
+    except Exception as e:
+        return jsonify({'response': str(e)}), 400
+    return jsonify({'response': 'Solicitação alterada'})
+
+@janitorial_bp.route('/<int:id>', methods=['DELETE'])
 @jwt_required()
 def delete_request(id):
-  try:
-    req=db.get_or_404(Janitorial,id)
-    db.session.delete(req)
-    db.session.commit()
-  except Exception as e:
-    return jsonify({'response':str(e)})
-  return jsonify({'response':'solicitacao excluida'})
+    try:
+        req = db.get_or_404(Janitorial, id)
+        db.session.delete(req)
+        db.session.commit()
+    except Exception as e:
+        return jsonify({'response': str(e)}), 400
+    return jsonify({'response': 'Solicitação excluída'})
