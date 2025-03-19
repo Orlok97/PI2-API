1052290f14fef67b472b783a82692707f97de9f8
Cristian <cristian.braz97@gmail.com>
2025-03-18 22:09:16 -0300

    adiciona novos testes para o endpointe citizen

--- tests/test_citizens.py
+++ tests/test_citizens.py
@@ -1,7 +1,36 @@
+import pytest
 from . import client
 
 def test_create_citizen(client):
     payload={"nome":"Ana", "email":"ana@gmail.com","telefone":"13-45621435","senha":"123"}
     response=client.post('/api/v1/citizen/',json=payload)
+    assert response.status_code == 200
+    print(response.get_json())
+
+@pytest.fixture
+def auth_token(client):
+    payload={"email":"ana@gmail.com", "senha":"123", "permission":"citizen"}
+    response=client.post('/api/v1/auth/',json=payload)
+    assert response.status_code == 200
+    return response.get_json()['token']
+
+def test_list_citizens(client, auth_token):
+    response=client.get('/api/v1/citizen/', headers={'Authorization':f"Bearer {auth_token}"})
+    assert response.status_code == 200
+    print(response.get_json())
+
+def test_get_citizen_by_id(client, auth_token):
+    response=client.get('/api/v1/citizen/3',headers={'Authorization':f"Bearer {auth_token}"})
+    assert response.status_code == 200
+    print(response.get_json())
+    
+def test_update_citizen(client, auth_token):
+    payload={"nome":"Ana Maria","telefone":"(13) 9954542-7844","senha":"123"}
+    response=client.put('/api/v1/citizen/3',headers={"Authorization":f"Bearer {auth_token}"}, data=payload)
+    assert response.status_code == 200
+    print(response.get_json())
+
+def test_delete_citizen(client, auth_token):
+    response=client.delete('/api/v1/citizen/1',headers={"Authorization":f"Bearer {auth_token}"})
     assert response.status_code == 200
     print(response.get_json())
